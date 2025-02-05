3900fbf768f66f3455e3a78096c5070dca46e9b7
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-02-05 22:49:45 +0000

    Add files via upload

--- forge_app.py
+++ forge_app.py
@@ -1,11 +1,14 @@
 import gradio as gr
 import torch
-from transformers import AutoConfig, AutoModelForCausalLM
-from transformers import BitsAndBytesConfig as TransformersBitsAndBytesConfig
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, DynamicCache
+from transformers import BitsAndBytesConfig
 
 from janus.models import MultiModalityCausalLM, VLChatProcessor
-from janus.utils.io import load_pil_images
+
 from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+from datetime import datetime
+
 
 import numpy as np
 import os
@@ -22,27 +25,34 @@ if use_7B:
     config = AutoConfig.from_pretrained(model_path)
     language_config = config.language_config
     language_config._attn_implementation = 'eager'
-    quant_config = TransformersBitsAndBytesConfig(load_in_8bit=True,)
-    vl_gpt = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=quant_config,
-                                                 trust_remote_code=False,
-                                                 torch_dtype=torch.bfloat16,)
-
-    vl_chat_processor = VLChatProcessor.from_pretrained(model_path, quantization_config=quant_config, 
+    
+    quant_config = BitsAndBytesConfig(load_in_4bit=True,) #load_in_8bit=True
+    vl_chat_processor = VLChatProcessor.from_pretrained(model_path, 
+                                                        quantization_config=quant_config, 
                                                         torch_dtype=torch.bfloat16,)
+    vl_gpt = AutoModelForCausalLM.from_pretrained(model_path, 
+                                                  quantization_config=quant_config,
+                                                  trust_remote_code=False,
+                                                  torch_dtype=torch.bfloat16,)
+
 else:
     model_path = "deepseek-ai/Janus-Pro-1B"
-    config = AutoConfig.from_pretrained(model_path)
+    config = AutoConfig.from_pretrained("deepseek-ai/Janus-Pro-7B")
     language_config = config.language_config
     language_config._attn_implementation = 'eager'
-    vl_gpt = MultiModalityCausalLM.from_pretrained(model_path,
+    vl_chat_processor = VLChatProcessor.from_pretrained(model_path, torch_dtype=torch.bfloat16,)
+    vl_gpt = AutoModelForCausalLM.from_pretrained(model_path,
                                                  torch_dtype=torch.bfloat16,).cuda()
 
-    vl_chat_processor = VLChatProcessor.from_pretrained(model_path, torch_dtype=torch.bfloat16,)
 
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 @torch.inference_mode()
-def multimodal_understanding(image, question, seed, top_p, temperature):
+def multimodal_understanding(image, question, seed, top_p, temperature, max_new_tokens):
     torch.cuda.empty_cache()
+    
+    if image is None:
+        return "You must provide an image to discuss."
     
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -61,30 +71,29 @@ def multimodal_understanding(image, question, seed, top_p, temperature):
     prepare_inputs = vl_chat_processor(
         conversations=conversation, images=pil_images, force_batchify=True
     ).to(device='cuda', dtype=torch.bfloat16)
-    
+
     inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
     
     outputs = vl_gpt.language_model.generate(
         inputs_embeds=inputs_embeds,
         attention_mask=prepare_inputs.attention_mask,
-        pad_token_id=vl_chat_processor.tokenizer.eos_token_id,
-        bos_token_id=vl_chat_processor.tokenizer.bos_token_id,
-        eos_token_id=vl_chat_processor.tokenizer.eos_token_id,
-        max_new_tokens=512,
+        pad_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        max_new_tokens=max_new_tokens,
         do_sample=False if temperature == 0 else True,
         use_cache=True,
         temperature=temperature,
         top_p=top_p,
     )
     
-    answer = vl_chat_processor.tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+    answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
     return answer
 
 @torch.inference_mode()
 def generate(input_ids,
              width,
-             height,# w*h = 576*x*x
-
+             height,
              temperature: float = 1,
              parallel_size: int = 5,
              cfg_weight: float = 5,
@@ -98,18 +107,20 @@ def generate(input_ids,
         tokens[i, :] = input_ids
         if i % 2 != 0:
             tokens[i, 1:-1] = vl_chat_processor.pad_id
-    inputs_embeds = vl_gpt.language_model.get_input_embeddings()(tokens)
+    inputs_embeds = vl_gpt.language_model.get_input_embeddings()(tokens).to('cuda')
     generated_tokens = torch.zeros((parallel_size, image_token_num_per_image), dtype=torch.int).to('cuda')
 
-
-
-    pkv = None
+    pkv = DynamicCache()
     for i in range(image_token_num_per_image):
         with torch.no_grad():
             outputs = vl_gpt.language_model.model(inputs_embeds=inputs_embeds,
                                                 use_cache=True,
                                                 past_key_values=pkv)
-            pkv = outputs.past_key_values
+            pkv = outputs.past_key_values #these exactly 24, perhaps this forces width to 384 else broken
+            # print (len(pkv)) # always 24
+            # while len(pkv) < width // patch_size:
+                # pkv.append(pkv[-1])
+
             hidden_states = outputs.last_hidden_state
             logits = vl_gpt.gen_head(hidden_states[:, -1, :])
             logit_cond = logits[0::2, :]
@@ -122,19 +133,21 @@ def generate(input_ids,
 
             img_embeds = vl_gpt.prepare_gen_img_embeds(next_token)
             inputs_embeds = img_embeds.unsqueeze(dim=1)
-
+   
+    # 25x24 -> 619
+    # 25x25 -> 644 (+25)
+    # 24x25 -> 619 (+24)
+    # 24x24 -> 595
     
-
     patches = vl_gpt.gen_vision_model.decode_code(generated_tokens.to(dtype=torch.int),
-                                                 shape=[parallel_size, 8, width // patch_size, height // patch_size])
+                                                 shape=[parallel_size, 8, height // patch_size, width // patch_size])
 
     return generated_tokens.to(dtype=torch.int), patches
 
-def unpack(dec, width, height, parallel_size=5):
+def unpack(dec):
     dec = dec.to(torch.float32).cpu().numpy().transpose(0, 2, 3, 1)
-    dec = np.clip((dec + 1) / 2 * 255, 0, 255)
+    dec = np.clip((dec + 1) / 2 * 255, 0, 255)  #potential different bitrate, unlikely but maybe worth investigating
 
-    # visual_img = np.zeros((parallel_size, width, height, 3), dtype=np.uint8)
     visual_img = np.zeros(dec.shape, dtype=np.uint8)
 
     visual_img[:, :, :] = dec
@@ -152,20 +165,18 @@ def generate_image(prompt,
                    width=384,
                    height=384,
                    patch_size=16):
-    # Clear CUDA cache and avoid tracking gradients
+
     torch.cuda.empty_cache()
-    
+
     seed = get_fixed_seed(-1 if random else seed)
 
     if seed is not None:
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
         np.random.seed(seed)
-    
+
     image_tokens = (width // patch_size) * (height // patch_size)
-    
-    parallel_size = image_count
-    
+
     with torch.no_grad():
         messages = [{'role': '<|User|>', 'content': prompt},
                     {'role': '<|Assistant|>', 'content': ''}]
@@ -174,32 +185,52 @@ def generate_image(prompt,
                                                                    system_prompt='')
         text = text + vl_chat_processor.image_start_tag
         
-        input_ids = torch.LongTensor(vl_chat_processor.tokenizer.encode(text))
+        input_ids = torch.LongTensor(tokenizer.encode(text))
         output, patches = generate(input_ids,
-                                   height // patch_size * patch_size,
                                    width // patch_size * patch_size,
+                                   height // patch_size * patch_size,
                                    cfg_weight=guidance,
-                                   parallel_size=parallel_size,
+                                   parallel_size=image_count,
                                    temperature=t2i_temperature,
                                    image_token_num_per_image=image_tokens,
                                    patch_size=patch_size)
-        images = unpack(patches,
-                        height // patch_size * patch_size,
-                        width // patch_size * patch_size,
-                        parallel_size=parallel_size)
+        images = unpack(patches)
 
-        return images, seed
+    model = "7B" if use_7B else "1B"
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    c = 1;
+    for i in images:
+        info = f"{prompt}\nSize: {width}x{height}, Seed: {seed}, CFG scale: {guidance}, Temperature: {t2i_temperature}, Batch: {c}/{image_count}, Model (Janus-Pro): {model}"
+        output_path = f"outputs\\Janus-Pro_{now}-{c}.png"
+
+        metadata = PngInfo()
+        metadata.add_text("parameters", info)
+        image = Image.fromarray(i)
+        image.save(output_path, format='PNG', pnginfo=metadata)
+        
+        c += 1
+
+    return images, seed
 
 def unload():
-    global vl_gpt, vl_chat_processor
-    del vl_gpt, vl_chat_processor
+    global vl_gpt, vl_chat_processor, tokenizer
+    del vl_gpt, vl_chat_processor, tokenizer
     gc.collect()
     torch.cuda.empty_cache()
 
 
-# Gradio interface
-with gr.Blocks(analytics_enabled=False) as demo:
-    gr.Markdown(value="# Janus Pro (1B)")
+css = """
+h1 {
+  text-align: center;
+}
+footer {
+    display: none !important;
+}
+"""
+
+
+with gr.Blocks(css=css, analytics_enabled=False) as demo:
+    gr.Markdown(value="# Janus-Pro")
     
     with gr.Tab("Multimodal Understanding"):
         with gr.Row():
@@ -207,6 +238,9 @@ with gr.Blocks(analytics_enabled=False) as demo:
                 image_input = gr.Image(height="70vh")
                 with gr.Row():
                     und_seed_input = gr.Number(label="Seed", precision=0, value=42)
+                    max_new_tokens = gr.Slider(label="Max. new tokens", minimum=128, maximum=1024, value=512, step=32)
+
+                with gr.Row():
                     top_p = gr.Slider(minimum=0, maximum=1, value=0.95, step=0.05, label="top_p")
                     temperature = gr.Slider(minimum=0.05, maximum=1, value=0.1, step=0.05, label="temperature")
 
@@ -223,12 +257,12 @@ with gr.Blocks(analytics_enabled=False) as demo:
 
                 question_input.submit(fn=toggleChatOff, outputs=[understanding_button]).then(
                     multimodal_understanding,
-                    inputs=[image_input, question_input, und_seed_input, top_p, temperature],
+                    inputs=[image_input, question_input, und_seed_input, top_p, temperature, max_new_tokens],
                     outputs=understanding_output
                 ).then(fn=toggleChatOn, outputs=[understanding_button])
                 understanding_button.click(fn=toggleChatOff, outputs=[understanding_button]).then(
                     multimodal_understanding,
-                    inputs=[image_input, question_input, und_seed_input, top_p, temperature],
+                    inputs=[image_input, question_input, und_seed_input, top_p, temperature, max_new_tokens],
                     outputs=understanding_output
                 ).then(fn=toggleChatOn, outputs=[understanding_button])
 
@@ -251,9 +285,6 @@ with gr.Blocks(analytics_enabled=False) as demo:
     with gr.Tab("Text-to-Image"):
         with gr.Row():
             with gr.Column():
-                with gr.Row():
-                    cfg_weight_input = gr.Slider(minimum=1, maximum=10, value=2, step=0.1, label="CFG Weight")
-                    t2i_temperature = gr.Slider(minimum=0.05, maximum=1.0, value=0.5, step=0.05, label="temperature")
 
                 prompt_input = gr.Textbox(label="Prompt (more detail can help produce better images!)")
                 with gr.Row():
@@ -265,8 +296,10 @@ with gr.Blocks(analytics_enabled=False) as demo:
                     width  = gr.Slider(label="Width (fixed)",  step=16, value=384, minimum=160, maximum=768, interactive=False)
                     height = gr.Slider(label="Height", step=16, value=384, minimum=160, maximum=768)
                     patch_size =  gr.Number(label="Patch size", step=1, value=16, scale=1, visible=False)   # seems like hardcoded to 16 somewhere
-                    # 576(tokens)=24*24; 24*16(patch) = 384(width, height)
-                    # changing size: broken incoherent output
+
+                with gr.Row():
+                    cfg_weight_input = gr.Slider(minimum=1, maximum=10, value=2, step=0.1, label="CFG Weight")
+                    t2i_temperature = gr.Slider(minimum=0.05, maximum=1.0, value=0.5, step=0.01, label="temperature")
 
                 examples_t2i = gr.Examples(
                     label="Text to image generation examples.",
